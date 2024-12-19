ee4b068899b6bfcbabf078011e8c0f10234c3cc7
unlucky123 <nilsanchezsenye@gmail.com>
2024-12-19 19:24:10 +0100

    Buscar numero
    Añadido la opción de buscar el numero

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,28 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		Scanner scan = new Scanner(System.in); 
+		int pepenum = 0;
+		boolean esta = false;
+		System.out.println("Que numero quieres encontrar?");
+		pepenum = scan.nextInt();
+		for (int i = 0; i < pepeMatriz.length; i++) {
+			for (int j = 0; j < pepeMatriz[0].length; j++) {
+				if(pepeMatriz[i][j] == pepenum) {
+					esta = true;
+				}
+			}
+		}
+		if(esta) {
+			System.out.println("El numero esta");
+		}
+		else {
+			System.out.println("No esta");
+		}
 	}
 
 }
