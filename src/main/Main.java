a740d98fbb496e497b876ad024b33f91cf6c8ffb
Your Name <you@example.com>
2024-12-19 19:16:25 +0100

    Matriz y num random

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.util.Iterator;
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		int[][] pepeMatriz = new int [5][5];
+		
+		Random rand = new Random();
+		
+		for (int i = 0; i < pepeMatriz.length; i++) {
+			for (int j = 0; j < pepeMatriz.length; j++) {
+				int peperand = rand.nextInt(101);
+				pepeMatriz[i][j] = peperand;
+			}
+		}
+		
 	}
 
 }
